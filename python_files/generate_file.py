426d4dfe47608f0c4052b900d26bfebc2f3c1d1d
shivkubrick <shivam.bhatnagar@genomicsengland.co.uk>
2021-10-26 13:18:04 +0100

    Refactor file structure

--- python_files/generate_file.py
+++ python_files/generate_file.py
@@ -53,35 +53,3 @@ p2.execute_bio_file()
 
 
 
-###### Uploading to Trifacta
-# plcm_date = os.environ.get("DATE")
-# plcm = plcm.PLCM(plcm_date)
-# plcm.create_gms_tracker_file()
-
-# token = ('eyJ0b2tlbklkIjoiMGI0NjRhZjItZmFjNS00MjczLTkxZmYtZDE2NWQyM2I1OTg1Iiwic2Vjcm'
-#             'V0IjoiNDlkOTdlOTQ5NDU4ZDAzZTk2OGVjYzM3ZWUyNjUyOWY2YjlhNTQwYjUyY2MyYzljZWYz'
-#             'ZTU3M2JmZmMwODU5MyJ9')
-# host='https://trifacta.clinical-data-wranglers.preprod.aws.gel.ac/'
-
-# trifacta_client = trifacta.TrifactaClient(host=host, token=token)
-# flows = trifacta_client.get_flows()
-# print(flows)
-
-# for row in rows:
-#     base = os.environ.get("BASE")
-#     fpath = base + f'/Data/Tables/{plcm_date}/{row}.csv' 
-
-#     new_id = trifacta_client.upload_dataset(fpath, row)
-
-#     # Get Flow Id
-#     flow_0_id = trifacta_client.get_flow_id('0 - Reference Files')
-#     flow_1_id = trifacta_client.get_flow_id('1 - Preparation')
-#     flow_2_id = trifacta_client.get_flow_id('2 - Output')
-
-#     trifacta_client.replace_dataset(flow_1_id, 14910, new_id)
-
-
-   
-
-
-
---
+++ sql/ngis_genomicrecord_ordering_entity.sql
@@ -0,0 +1,2 @@
+select referral_human_readable_stored_id, ordering_entity_uid 
+from referral r 
